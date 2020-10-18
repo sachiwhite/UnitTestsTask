7ba30f87173e4a197e6ebf9d9c400ab6e9ead316
Blanka Lewonowska <loczkovata@gmail.com>
2020-10-18 23:43:37 +0200

    working solution

---
+++ UnitTestProject1/LukeSkywalkerTask.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+    public class SW_Person_Data    
+    {
+        public string name { get; set; } 
+        public string homeworld { get; set; } 
+    }
+
+    public class SW_Planet_Data
+    {
+        public string Name { get; set; } 
+    }
+    [TestFixture]
+    public class LukeSkywalkerTask
+    {
+        private HttpClient client;
+        
+        [SetUp]
+        public void SetupTest()
+        {
+            client = new HttpClient();
+        }
+
+        [Test]
+        public async Task IsLukeSkywalkerFromTatooineTest_ReturnsTrue()
+        {
+            var path = @"https://swapi.dev/api/people/1/";
+            HttpResponseMessage response = await client.GetAsync(path);
+            var person = await response.Content.ReadAsAsync<SW_Person_Data>();
+            response = await client.GetAsync(person.homeworld);
+            var planet = await response.Content.ReadAsAsync<SW_Planet_Data>();
+            Assert.AreEqual(planet.Name, "Tatooine");
+        }
+    }
+}
---
+++ UnitTestProject1/MapsTask.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using Serilog;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    class MapsTask
+    {
+        private IWebDriver chromeDriver;
+        private IWebDriver firefoxDriver;
+
+        [SetUp]
+        public void SetupTests()
+        {
+            chromeDriver = new ChromeDriver(@"C:\drivers\");
+            firefoxDriver = new FirefoxDriver(@"C:\drivers\");
+            var mapsUrl =  @"https://www.google.pl/maps/";
+            chromeDriver.Url = mapsUrl;
+            List<IWebDriver> drivers = new List<IWebDriver>(){chromeDriver, firefoxDriver};
+            foreach (var driver in drivers)
+            {
+                var consentUrl = driver.FindElement(By.ClassName("widget-consent-frame")).GetAttribute("src");
+                driver.Navigate().GoToUrl(consentUrl);
+                var consent = driver.FindElement(By.Id("introAgreeButton"));
+                consent.Click();
+                driver.Navigate().GoToUrl(mapsUrl);
+                var routeButton = chromeDriver.FindElement(By.Id("searchbox")).FindElement(By.ClassName("searchbox-directions-container")).FindElement(By.TagName("button"));
+                routeButton.Click();
+                
+
+            }
+            
+        }
+        [Test]
+        public void RoadToOfficeTakesLessThan40MinutesAndLessThan3km_OnFoot()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void RoadToOfficeTakesLessThan40MinutesAndLessThan3km_ByBike()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void RoadFromOfficeTakesLessThan40MinutesAndLessThan3km_OnFoot()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void RoadFromOfficeTakesLessThan40MinutesAndLessThan3km_ByBike()
+        {
+            Assert.Fail();
+            
+        }
+
+
+    }
+}
